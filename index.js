4c30f7cb9b51df72e21710565098468fbd6e00ac
Nedas-M <nedas.mikucionis123@gmail.com>
2024-10-06 22:02:34 +0100

    making models/ routes/ config/

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
 
 const app = express();
 
 const port = process.envPORT || 3000;
 
 app.use(express.json());
-
-mongoose.connect('mongodb://localhost:27017/ecommerce', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.get('/', (req, res) => {
     res.json({ message: 'Welcome to the e-commerce platform API' });
